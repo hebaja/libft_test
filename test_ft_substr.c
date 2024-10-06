0d7547dc774e1391d0158fcace4740b6ca82edb1
hebatist <hebaja@yahoo.com.br>
2024-10-06 19:54:05 -0300

    test_ft_substr update

--- test_ft_substr.c
+++ test_ft_substr.c
@@ -5,20 +5,31 @@ void	test_ft_substr()
 {
 	int	result;
 	int	output;
-	char	*str = "***Nevermore***";
+	int	size;
+	char	*str = "Nevermore";
 	char	*ptr;
-	ptr = ft_substr(str, 3, 9);
 	printf(">>> FT_SUBSTR: ");
 	result = 1;
+	ptr = ft_substr(str, 0, 9);
 	output = ft_strncmp(ptr, "Nevermore", 9);
 	if (output)
 		result = 0;
-	output = ft_strncmp(ptr, "moreNever", 9);
-	if (!output)
+	ptr = ft_substr(str, 5, 4);
+	output = ft_strncmp(ptr, "more", 4);
+	if (output)
 		result = 0;
-	ptr = ft_substr(str, 10, 15);
-	if (ptr != NULL)
+	ptr = ft_substr(str, 11, 5);
+	output = ft_strncmp(ptr, "", 1);
+	if (output)
 		result = 0;
+	ptr = ft_substr(str, 5, 6);
+	output = ft_strncmp(ptr, "more", 4);
+	if (output)
+		result = 0;
+	size = ft_strlen(ptr);
+	if (size != 4)
+		result = 0;
+
 	show(result);
 	free(ptr);
 }
